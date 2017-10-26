6c1a3c2cbf04bbd044612fa8090c098ea671acb5
Clivern <hello@clivern.com>
2017-10-26 15:48:54 +0200

    base sender and receiver added

--- src/main/java/com/clivern/fred/receivers/BaseReceiver.java
+++ src/main/java/com/clivern/fred/receivers/BaseReceiver.java
@@ -3,10 +3,33 @@
  */
 package com.clivern.fred.receivers;
 
+import com.clivern.fred.receivers.webhook.*;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.utils.Config;
+import com.clivern.fred.utils.Log;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 /**
  * Base Receiver Class
  */
 public class BaseReceiver {
 
+    protected Config configs;
+
+    protected Log log;
+
+
+    /**
+     * Class Constructor
+     *
+     * @param configs
+     * @param log
+     */
+    public BaseReceiver(Config configs, Log log)
+    {
+        this.configs = configs;
+        this.log = log;
+    }
 }
--- src/main/java/com/clivern/fred/senders/BaseSender.java
+++ src/main/java/com/clivern/fred/senders/BaseSender.java
@@ -3,10 +3,51 @@
  */
 package com.clivern.fred.senders;
 
+import com.clivern.fred.senders.templates.*;
+import com.clivern.fred.utils.Config;
+import com.clivern.fred.utils.Log;
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.options.Options;
+import com.mashape.unirest.request.GetRequest;
+import com.mashape.unirest.request.HttpRequest;
 
 /**
  * Base Sender Class
  */
 public class BaseSender {
 
+    protected Config configs;
+
+    protected Log log;
+
+
+    /**
+     * Class Constructor
+     *
+     * @param configs
+     * @param log
+     */
+    public BaseSender(Config configs, Log log)
+    {
+        this.configs = configs;
+        this.log = log;
+    }
+
+    /**
+     * Send Plain Message JSON
+     *
+     * @param  body
+     * @return Boolean
+     * @throws UnirestException
+     */
+    public Boolean send(String body) throws UnirestException
+    {
+        String url = this.remote_url + this.configs.get("page_access_token", "");
+        this.log.info("curl -X POST -H \"Content-Type: application/json\" -d '" + body + "' \"" + url + "\"");
+        HttpResponse<String> response = Unirest.post(url).header("Content-Type", "application/json").body(body).asString();
+
+        return true;
+    }
 }
