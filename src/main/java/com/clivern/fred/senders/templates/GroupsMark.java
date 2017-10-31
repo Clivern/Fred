2ade1478eb9171082dd3f893868ad61331dfcbf8
Clivern <hello@clivern.com>
2017-10-31 15:15:38 +0200

    Groups Mark template added

---
+++ src/main/java/com/clivern/fred/senders/templates/GroupsMark.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (C) 2017 Clivern. <https://clivern.com>
+ */
+package com.clivern.fred.senders.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.configs.Basic;
+import com.clivern.fred.contract.templates.BasicTemplate;
+
+/**
+ * Groups Mark Template Class
+ *
+ * This Class Moves the read cursor in a private channel.
+ *
+ * For More Info, Please Visit <https://api.slack.com/methods/groups.mark>
+ */
+public class GroupsMark implements BasicTemplate {
+
+    protected String url = Basic.methodURL(Basic.groupsMarkMethod);
+    protected String body = "";
+    protected String contentType = "application/x-www-form-urlencoded";
+    protected String method = "POST";
+    protected String token;
+    protected String channel;
+    protected String ts;
+    protected Boolean valid;
+
+    /**
+     * Set URL
+     *
+     * @param  url
+     */
+    public void setURL(String url)
+    {
+        this.url = url;
+    }
+
+    /**
+     * Set Message Body
+     *
+     * @param  body
+     */
+    public void setBody(String body)
+    {
+        this.body = body;
+    }
+
+    /**
+     * Set Content Type
+     *
+     * @param contentType
+     */
+    public void setContentType(String contentType)
+    {
+        this.contentType = contentType;
+    }
+
+    /**
+     * Set Method
+     *
+     * @param method
+     */
+    public void setMethod(String method)
+    {
+        this.method = method;
+    }
+
+    /**
+     * Set Token
+     *
+     * @param token
+     */
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    /**
+     * Set Channel
+     *
+     * @param channel
+     */
+    public void setChannel(String channel)
+    {
+        this.channel = channel;
+    }
+
+    /**
+     * Set Ts
+     *
+     * @param ts
+     */
+    public void setTs(String ts)
+    {
+        this.ts = ts;
+    }
+
+    /**
+     * Get URL
+     *
+     * @return String
+     */
+    public String getURL()
+    {
+        return this.url;
+    }
+
+    /**
+     * Get Body
+     *
+     * @return String
+     */
+    public String getBody()
+    {
+        return this.body;
+    }
+
+    /**
+     * Get Content Type
+     *
+     * @return String
+     */
+    public String getContentType()
+    {
+        return this.contentType;
+    }
+
+    /**
+     * Get Method
+     *
+     * @return String
+     */
+    public String getMethod()
+    {
+        return this.method;
+    }
+
+    /**
+     * Get Token
+     *
+     * @return String
+     */
+    public String getToken()
+    {
+        return this.token;
+    }
+
+    /**
+     * Get Channel
+     *
+     * @return String
+     */
+    public String getChannel()
+    {
+        return this.channel;
+    }
+
+    /**
+     * Get Ts
+     *
+     * @return String
+     */
+    public void getTs()
+    {
+        return this.ts;
+    }
+
+    /**
+     * Build Message Body
+     */
+    public String build()
+    {
+        if( this.token.isEmpty() || this.channel.isEmpty() || this.ts.isEmpty() ){
+            return "";
+        }
+
+        if( !this.token.isEmpty() ){
+            this.body += "token=" + this.token;
+        }
+
+        if( !this.channel.isEmpty() ){
+            this.body += "&channel=" + this.channel;
+        }
+
+        if( !this.ts.isEmpty() ){
+            this.body += "&ts=" + this.ts;
+        }
+
+        return this.body;
+    }
+
+    /**
+     * Check if All Required Data is Provided
+     *
+     * @return Boolean
+     */
+    public Boolean isValid()
+    {
+        return (!this.method.isEmpty() && !this.contentType.isEmpty() && !this.body.isEmpty() && !this.url.isEmpty());
+    }
+
+    /**
+     * Debug The Request
+     *
+     * @return String
+     */
+    public String debug()
+    {
+        return "curl -X " + this.method + " -H \"Content-Type: " + this.contentType + "\" -d '" + this.body + "' \"" + this.url + "\"";
+    }
+}
