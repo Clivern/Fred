106d9d937d775d5709184e1931502d6c3b42a596
Clivern <hello@clivern.com>
2017-10-31 17:31:32 +0200

    License Terms added to classes

--- src/main/java/com/clivern/fred/senders/templates/AppsPermissionsInfo.java
+++ src/main/java/com/clivern/fred/senders/templates/AppsPermissionsInfo.java
@@ -1,5 +1,15 @@
 /*
- * Copyright (C) 2017 Clivern. <https://clivern.com>
+ * Copyright (C) 2017 A.F <http://clivern.com>
+ *
+ * Licensed under the MIT License; you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://github.com/Clivern/Fred/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.clivern.fred.senders.templates;
 
@@ -12,7 +22,12 @@ import com.clivern.fred.contract.templates.BasicTemplate;
 /**
  * Apps Permissions Info Template Class
  *
- * For More Info, Please Visit <https://api.slack.com/methods/apps.permissions.info>
+ * It returns list of permissions this app has on a team.
+ *
+ * <a href="https://api.slack.com/methods/apps.permissions.info">For More Info</a>
+ *
+ * @author A.F
+ * @since 1.0.0
  */
 public class AppsPermissionsInfo implements BasicTemplate {
 
