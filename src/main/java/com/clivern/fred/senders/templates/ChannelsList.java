0a5019b80094b5870861c36a310a0bda9683e813
Clivern <hello@clivern.com>
2017-10-31 13:29:09 +0200

    channels list template added

---
+++ src/main/java/com/clivern/fred/senders/templates/ChannelsList.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (C) 2017 Clivern. <https://clivern.com>
+ */
+package com.clivern.fred.senders.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.configs.Basic;
+import com.clivern.fred.contract.templates.BasicTemplate;
+
+/**
+ * Channels List Template Class
+ *
+ * For More Info, Please Visit <https://api.slack.com/methods/channels.list>
+ */
+public class ChannelsList implements BasicTemplate {
+
+    protected String url = Basic.methodURL(Basic.channelsListMethod);
+    protected String body = "";
+    protected String contentType = "application/x-www-form-urlencoded";
+    protected String method = "POST";
+    protected String token;
+    protected String cursor;
+    protected Boolean excludeArchived;
+    protected Boolean excludeMembers;
+    protected Integer limit;
+    protected Boolean valid;
+
+
+    /**
+     * Set URL
+     *
+     * @param  url
+     */
+    public void setURL(String url)
+    {
+        this.url = url;
+    }
+
+    /**
+     * Set Message Body
+     *
+     * @param  body
+     */
+    public void setBody(String body)
+    {
+        this.body = body;
+    }
+
+    /**
+     * Set Content Type
+     *
+     * @param contentType
+     */
+    public void setContentType(String contentType)
+    {
+        this.contentType = contentType;
+    }
+
+    /**
+     * Set Method
+     *
+     * @param method
+     */
+    public void setMethod(String method)
+    {
+        this.method = method;
+    }
+
+    /**
+     * Set Token
+     *
+     * @param token
+     */
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    /**
+     * Set Cursor
+     *
+     * @param cursor
+     */
+    public void setCursor(String cursor)
+    {
+        this.cursor = cursor;
+    }
+
+    /**
+     * Set Exclude Archived
+     *
+     * @param excludeArchived
+     */
+    public void setExcludeArchived(Boolean excludeArchived)
+    {
+        this.excludeArchived = excludeArchived;
+    }
+
+    /**
+     * Set Exclude Members
+     *
+     * @param excludeMembers
+     */
+    public void setExcludeMembers(Boolean excludeMembers)
+    {
+        this.excludeMembers = excludeMembers;
+    }
+
+    /**
+     * Set Limit
+     *
+     * @param limit
+     */
+    public void setLimit(Integer limit)
+    {
+        this.limit = limit;
+    }
+
+    /**
+     * Get URL
+     *
+     * @return String
+     */
+    public String getURL()
+    {
+        return this.url;
+    }
+
+    /**
+     * Get Body
+     *
+     * @return String
+     */
+    public String getBody()
+    {
+        return this.body;
+    }
+
+    /**
+     * Get Content Type
+     *
+     * @return String
+     */
+    public String getContentType()
+    {
+        return this.contentType;
+    }
+
+    /**
+     * Get Method
+     *
+     * @return String
+     */
+    public String getMethod()
+    {
+        return this.method;
+    }
+
+    /**
+     * Get Token
+     *
+     * @return String
+     */
+    public String getToken()
+    {
+        return this.token;
+    }
+
+    /**
+     * Get Cursor
+     *
+     * @return String
+     */
+    public String getCursor()
+    {
+        return this.cursor;
+    }
+
+    /**
+     * Get Exclude Archived
+     *
+     * @return Boolean
+     */
+    public Boolean getExcludeArchived()
+    {
+        return this.excludeArchived;
+    }
+
+    /**
+     * Get Exclude Members
+     *
+     * @return Boolean
+     */
+    public Boolean getExcludeMembers()
+    {
+        return this.excludeMembers;
+    }
+
+    /**
+     * Get Limit
+     *
+     * @return Integer
+     */
+    public Integer getLimit()
+    {
+        return this.limit;
+    }
+
+    /**
+     * Build Message Body
+     */
+    public String build()
+    {
+        if( this.token.isEmpty() ){
+            return "";
+        }
+
+        if( !this.token.isEmpty() ){
+            this.body += "token=" + this.token;
+        }
+
+        if( !this.cursor.isEmpty() ){
+            this.body += "&cursor=" + this.cursor;
+        }
+
+        if( !this.excludeArchived !== null ){
+            this.body += "&exclude_archived=" + ((this.excludeArchived) ? "true" : "false");
+        }
+
+        if( !this.excludeMembers !== null ){
+            this.body += "&exclude_members=" + ((this.excludeMembers) ? "true" : "false");
+        }
+
+        if( !this.limit.isEmpty() ){
+            this.body += "&limit=" + this.limit;
+        }
+
+        return this.body;
+    }
+
+    /**
+     * Check if All Required Data is Provided
+     *
+     * @return Boolean
+     */
+    public Boolean isValid()
+    {
+        return (!this.method.isEmpty() && !this.contentType.isEmpty() && !this.body.isEmpty() && !this.url.isEmpty());
+    }
+
+    /**
+     * Debug The Request
+     *
+     * @return String
+     */
+    public String debug()
+    {
+        return "curl -X " + this.method + " -H \"Content-Type: " + this.contentType + "\" -d '" + this.body + "' \"" + this.url + "\"";
+    }
+}
