1594a3d5142ffe1dc0bcdfc15296567117acefee
Clivern <hello@clivern.com>
2017-11-02 13:26:26 +0200

    Coversations Create template added

---
+++ src/main/java/com/clivern/fred/senders/templates/ConversationsCreate.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2017 A.F <http://clivern.com>
+ *
+ * Licensed under the MIT License; you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://github.com/Clivern/Fred/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.clivern.fred.senders.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.configs.Basic;
+import com.clivern.fred.contract.templates.BasicTemplate;
+
+/**
+ * Conversations Create Template Class
+ *
+ * It Initiates a public or private channel-based conversation.
+ *
+ * <a href="https://api.slack.com/methods/conversations.create">For More Info</a>
+ *
+ * @author A.F
+ * @since 1.0.0
+ */
+public class ConversationsCreate implements BasicTemplate {
+
+    protected String url = Basic.methodURL(Basic.conversationsCreateMethod);
+    protected String body = "";
+    protected String contentType = "application/x-www-form-urlencoded";
+    protected String method = "POST";
+    protected String token;
+    protected String name;
+    protected Boolean isPrivate;
+    protected Boolean valid;
+
+    /**
+     * Set URL
+     *
+     * @param  url
+     */
+    public void setURL(String url)
+    {
+        this.url = url;
+    }
+
+    /**
+     * Set Message Body
+     *
+     * @param  body
+     */
+    public void setBody(String body)
+    {
+        this.body = body;
+    }
+
+    /**
+     * Set Content Type
+     *
+     * @param contentType
+     */
+    public void setContentType(String contentType)
+    {
+        this.contentType = contentType;
+    }
+
+    /**
+     * Set Method
+     *
+     * @param method
+     */
+    public void setMethod(String method)
+    {
+        this.method = method;
+    }
+
+    /**
+     * Set Token
+     *
+     * @param token
+     */
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    /**
+     * Set Name
+     *
+     * @param name
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Set Is Private
+     *
+     * @param isPrivate
+     */
+    public void setIsPrivate(Boolean isPrivate)
+    {
+        this.isPrivate = isPrivate;
+    }
+
+    /**
+     * Get URL
+     *
+     * @return String
+     */
+    public String getURL()
+    {
+        return this.url;
+    }
+
+    /**
+     * Get Body
+     *
+     * @return String
+     */
+    public String getBody()
+    {
+        return this.body;
+    }
+
+    /**
+     * Get Content Type
+     *
+     * @return String
+     */
+    public String getContentType()
+    {
+        return this.contentType;
+    }
+
+    /**
+     * Get Method
+     *
+     * @return String
+     */
+    public String getMethod()
+    {
+        return this.method;
+    }
+
+    /**
+     * Get Token
+     *
+     * @return String
+     */
+    public String getToken()
+    {
+        return this.token;
+    }
+
+    /**
+     * Get Name
+     *
+     * @return String
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Get Is Private
+     *
+     * @return Boolean
+     */
+    public Boolean getIsPrivate()
+    {
+        return this.isPrivate;
+    }
+
+    /**
+     * Build Message Body
+     */
+    public String build()
+    {
+        if( this.token.isEmpty() || this.name.isEmpty() ){
+            return "";
+        }
+
+        if( !this.token.isEmpty() ){
+            this.body += "token=" + this.token;
+        }
+
+        if( !this.name.isEmpty() ){
+            this.body += "&name=" + this.name;
+        }
+
+        if( !this.isPrivate !== null ){
+            this.body += "&is_private=" + ((this.isPrivate) ? "true" : "false");
+        }
+
+        return this.body;
+    }
+
+    /**
+     * Check if All Required Data is Provided
+     *
+     * @return Boolean
+     */
+    public Boolean isValid()
+    {
+        return (!this.method.isEmpty() && !this.contentType.isEmpty() && !this.body.isEmpty() && !this.url.isEmpty());
+    }
+
+    /**
+     * Debug The Request
+     *
+     * @return String
+     */
+    public String debug()
+    {
+        return "curl -X " + this.method + " -H \"Content-Type: " + this.contentType + "\" -d '" + this.body + "' \"" + this.url + "\"";
+    }
+
+    /**
+     * Debug The Request
+     *
+     * @return String
+     */
+    public String toString()
+    {
+        return this.debug();
+    }
+}
