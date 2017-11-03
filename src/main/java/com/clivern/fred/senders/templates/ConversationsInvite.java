fb6807268a86a1d1041f6f0759f0ea6f1968d8ec
Clivern <hello@clivern.com>
2017-11-03 15:36:57 +0200

    conversations invite updated

--- src/main/java/com/clivern/fred/senders/templates/ConversationsInvite.java
+++ src/main/java/com/clivern/fred/senders/templates/ConversationsInvite.java
@@ -22,63 +22,23 @@ import com.clivern.fred.contract.templates.BasicTemplate;
 /**
  * Conversations Invite Template Class
  *
- * It Invites users to a channel.
+ * It Invites users to a channel. It invites 1-30 users to a public or private channel.
+ * The calling user must be a member of the channel.
+ *
+ * Supported token types: user
+ * Expected scopes: channels:write, groups:write, im:write, mpim:write, post
  *
  * <a href="https://api.slack.com/methods/conversations.invite">For More Info</a>
  *
  * @author A.F
  * @since 1.0.0
  */
-public class ConversationsInvite implements BasicTemplate {
+public class ConversationsInvite extends BasicTemplate {
 
     protected String url = Basic.methodURL(Basic.conversationsInviteMethod);
-    protected String body = "";
-    protected String contentType = "application/x-www-form-urlencoded";
-    protected String method = "POST";
     protected String token;
     protected String channel;
     protected String users;
-    protected Boolean valid;
-
-    /**
-     * Set URL
-     *
-     * @param  url
-     */
-    public void setURL(String url)
-    {
-        this.url = url;
-    }
-
-    /**
-     * Set Message Body
-     *
-     * @param  body
-     */
-    public void setBody(String body)
-    {
-        this.body = body;
-    }
-
-    /**
-     * Set Content Type
-     *
-     * @param contentType
-     */
-    public void setContentType(String contentType)
-    {
-        this.contentType = contentType;
-    }
-
-    /**
-     * Set Method
-     *
-     * @param method
-     */
-    public void setMethod(String method)
-    {
-        this.method = method;
-    }
 
     /**
      * Set Token
@@ -108,46 +68,6 @@ public class ConversationsInvite implements BasicTemplate {
     public void setUsers(String users)
     {
         this.users = users;
-    }
-
-    /**
-     * Get URL
-     *
-     * @return String
-     */
-    public String getURL()
-    {
-        return this.url;
-    }
-
-    /**
-     * Get Body
-     *
-     * @return String
-     */
-    public String getBody()
-    {
-        return this.body;
-    }
-
-    /**
-     * Get Content Type
-     *
-     * @return String
-     */
-    public String getContentType()
-    {
-        return this.contentType;
-    }
-
-    /**
-     * Get Method
-     *
-     * @return String
-     */
-    public String getMethod()
-    {
-        return this.method;
     }
 
     /**
@@ -202,35 +122,5 @@ public class ConversationsInvite implements BasicTemplate {
         }
 
         return this.body;
-    }
-
-    /**
-     * Check if All Required Data is Provided
-     *
-     * @return Boolean
-     */
-    public Boolean isValid()
-    {
-        return (!this.method.isEmpty() && !this.contentType.isEmpty() && !this.body.isEmpty() && !this.url.isEmpty());
-    }
-
-    /**
-     * Debug The Request
-     *
-     * @return String
-     */
-    public String debug()
-    {
-        return "curl -X " + this.method + " -H \"Content-Type: " + this.contentType + "\" -d '" + this.body + "' \"" + this.url + "\"";
-    }
-
-    /**
-     * Debug The Request
-     *
-     * @return String
-     */
-    public String toString()
-    {
-        return this.debug();
     }
 }
