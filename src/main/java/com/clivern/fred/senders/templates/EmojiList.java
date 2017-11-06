d85a92b45a10136a4348c230f02e657e39ef7e1c
Clivern <hello@clivern.com>
2017-11-06 14:49:06 +0200

    Emoji Related Templates Fixed

---
+++ src/main/java/com/clivern/fred/senders/templates/EmojiList.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 A.F <http://clivern.com>
+ *
+ * Licensed under the MIT License; you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://github.com/Clivern/Fred/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.clivern.fred.senders.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.configs.Basic;
+import com.clivern.fred.contract.templates.BasicTemplate;
+
+/**
+ * Emoji List Template Class
+ *
+ * It Lists custom emoji for a team.
+ *
+ * Supported token types: bot, workspace, user
+ * Expected scopes: emoji:read, read
+ *
+ * <a href="https://api.slack.com/methods/emoji.list">For More Info</a>
+ *
+ * @author A.F
+ * @since 1.0.0
+ */
+public class EmojiList extends BasicTemplate {
+
+    protected String url = Basic.methodURL(Basic.emojiListMethod);
+
+    /**
+     * Build Message Body
+     */
+    public String build()
+    {
+        return "";
+    }
+}
