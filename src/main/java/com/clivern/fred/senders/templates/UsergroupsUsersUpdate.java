28899c742987f749613130313a0b38a9283d8770
Clivern <hello@clivern.com>
2017-11-06 15:11:58 +0200

    User groups Users Update updated

--- src/main/java/com/clivern/fred/senders/templates/UsergroupsUsersUpdate.java
+++ src/main/java/com/clivern/fred/senders/templates/UsergroupsUsersUpdate.java
@@ -35,12 +35,116 @@ import com.clivern.fred.contract.templates.BasicTemplate;
 public class UsergroupsUsersUpdate extends BasicTemplate {
 
     protected String url = Basic.methodURL(Basic.usergroupsUsersUpdateMethod);
+    protected String token;
+    protected String usergroup;
+    protected String users;
+    protected Boolean includeCount;
+
+    /**
+     * Set Token
+     *
+     * @param token
+     */
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    /**
+     * Set User Group
+     *
+     * @param usergroup
+     */
+    public void setUserGroup(String usergroup)
+    {
+        this.usergroup = usergroup;
+    }
+
+    /**
+     * Set Users
+     *
+     * @param users
+     */
+    public void setUsers(String users)
+    {
+        this.users = users;
+    }
+
+    /**
+     * Set Include Count
+     *
+     * @param includeCount
+     */
+    public void setIncludeCount(Boolean includeCount)
+    {
+        this.includeCount = includeCount;
+    }
+
+    /**
+     * Get Token
+     *
+     * @return String
+     */
+    public String getToken()
+    {
+        return this.token;
+    }
+
+    /**
+     * Get User Group
+     *
+     * @return String
+     */
+    public String getUserGroup()
+    {
+        return this.usergroup;
+    }
+
+    /**
+     * Get Users
+     *
+     * @return String
+     */
+    public String getUsers()
+    {
+        return this.users;
+    }
+
+    /**
+     * Get Include Count
+     *
+     * @return Boolean
+     */
+    public Boolean getIncludeCount()
+    {
+        return this.includeCount;
+    }
 
     /**
      * Build Message Body
      */
     public String build()
     {
-        return "";
+        if( this.token.isEmpty() || this.usergroup.isEmpty() || this.users.isEmpty() ){
+            return "";
+        }
+
+        if( !this.token.isEmpty() ){
+            this.body += "token=" + this.token;
+        }
+
+        if( !this.usergroup.isEmpty() ){
+            this.body += "&usergroup=" + this.usergroup;
+        }
+
+        if( !this.users.isEmpty() ){
+            this.body += "users=" + this.users;
+        }
+
+        if( this.includeCount != null ){
+            this.body += "&include_count=" + ((this.includeCount) ? "true" : "false");
+        }
+
+        return this.body;
     }
 }
