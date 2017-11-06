3a44fc4fbc32bb835dec8b81c085308df9429e4f
Clivern <hello@clivern.com>
2017-11-06 14:48:08 +0200

    Rtm Related Templates Fixed

---
+++ src/main/java/com/clivern/fred/senders/templates/RtmConnect.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 A.F <http://clivern.com>
+ *
+ * Licensed under the MIT License; you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://github.com/Clivern/Fred/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.clivern.fred.senders.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.configs.Basic;
+import com.clivern.fred.contract.templates.BasicTemplate;
+
+/**
+ * Rtm Connect Template Class
+ *
+ * It Starts a Real Time Messaging session.
+ *
+ * Supported token types: bot, workspace, user
+ * Expected scopes: rtm:stream, client
+ *
+ * <a href="https://api.slack.com/methods/rtm.connect">For More Info</a>
+ *
+ * @author A.F
+ * @since 1.0.0
+ */
+public class RtmConnect extends BasicTemplate {
+
+    protected String url = Basic.methodURL(Basic.rtmConnectMethod);
+
+    /**
+     * Build Message Body
+     */
+    public String build()
+    {
+        return "";
+    }
+}
