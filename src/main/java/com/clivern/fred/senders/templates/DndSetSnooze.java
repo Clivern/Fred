f6080c685a28ebd0c0fcd3eb9dc74a7cb79fe6ad
Clivern <hello@clivern.com>
2017-11-06 14:47:52 +0200

    Dnd Related Templates Fixed

---
+++ src/main/java/com/clivern/fred/senders/templates/DndSetSnooze.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 A.F <http://clivern.com>
+ *
+ * Licensed under the MIT License; you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://github.com/Clivern/Fred/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.clivern.fred.senders.templates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.clivern.fred.configs.Basic;
+import com.clivern.fred.contract.templates.BasicTemplate;
+
+/**
+ * Dnd Set Snooze Template Class
+ *
+ * It Turns on Do Not Disturb mode for the current user, or changes its duration.
+ *
+ * Supported token types: user
+ * Expected scopes: dnd:write, post
+ *
+ * <a href="https://api.slack.com/methods/dnd.setSnooze">For More Info</a>
+ *
+ * @author A.F
+ * @since 1.0.0
+ */
+public class DndSetSnooze extends BasicTemplate {
+
+    protected String url = Basic.methodURL(Basic.dndSetSnoozeMethod);
+
+    /**
+     * Build Message Body
+     */
+    public String build()
+    {
+        return "";
+    }
+}
