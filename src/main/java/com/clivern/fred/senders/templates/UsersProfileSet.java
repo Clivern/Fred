94be3e1ab38d47dc34ec75e1f2d01f3f75e424d8
Clivern <hello@clivern.com>
2017-11-10 19:43:51 +0200

    Users Profile Set Template Class Added

--- src/main/java/com/clivern/fred/senders/templates/UsersProfileSet.java
+++ src/main/java/com/clivern/fred/senders/templates/UsersProfileSet.java
@@ -35,12 +35,116 @@ import com.clivern.fred.contract.templates.BasicTemplate;
 public class UsersProfileSet extends BasicTemplate {
 
     protected String url = Basic.methodURL(Basic.usersProfileSetMethod);
+    protected String token;
+    protected String user;
+    protected String name;
+    protected String value;
+
+    /**
+     * Set Token
+     *
+     * @param token
+     */
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    /**
+     * Set User
+     *
+     * @param user
+     */
+    public void setUser(String user)
+    {
+        this.user = user;
+    }
+
+    /**
+     * Set Name
+     *
+     * @param name
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Set Value
+     *
+     * @param value
+     */
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+
+    /**
+     * Get Token
+     *
+     * @return String
+     */
+    public String getToken()
+    {
+        return this.token;
+    }
+
+    /**
+     * Get User
+     *
+     * @return String
+     */
+    public String getUser()
+    {
+        return this.user;
+    }
+
+    /**
+     * Get Name
+     *
+     * @return String
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Get Value
+     *
+     * @return String
+     */
+    public String getValue()
+    {
+        return this.value;
+    }
 
     /**
      * Build Message Body
      */
     public String build()
     {
-        return "";
+        if( this.token.isEmpty() ){
+            return "";
+        }
+
+        if( !this.token.isEmpty() ){
+            this.body += "token=" + this.token;
+        }
+
+        if( !this.user.isEmpty() ){
+            this.body += "&user=" + this.user;
+        }
+
+        if( !this.name.isEmpty() ){
+            this.body += "&name=" + this.name;
+        }
+
+        if( !this.value.isEmpty() ){
+            this.body += "&value=" + this.value;
+        }
+
+        return this.body;
     }
 }
