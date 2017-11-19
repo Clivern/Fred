cfe189a29972a74019b225048a4cb0f1a2c4ff28
Clivern <hello@clivern.com>
2017-11-19 15:37:21 +0200

    License updated to Apache 2

--- src/main/java/com/clivern/fred/sender/template/ConversationsArchive.java
+++ src/main/java/com/clivern/fred/sender/template/ConversationsArchive.java
@@ -1,10 +1,10 @@
 /*
  * Copyright (C) 2017 A.F <http://clivern.com>
  *
- * Licensed under the MIT License; you may not use this file except
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
- * https://github.com/Clivern/Fred/blob/master/LICENSE
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
