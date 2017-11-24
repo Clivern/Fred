634a5837181a2c1db0138f6430299a92c16a5687
Clivern <hello@clivern.com>
2017-11-24 21:49:41 +0200

    ConversationsKick Template Test Case Added

--- src/test/java/com/clivern/fred/sender/template/ConversationsKickTest.java
+++ src/test/java/com/clivern/fred/sender/template/ConversationsKickTest.java
@@ -20,13 +20,13 @@ public class ConversationsKickTest extends TestCase {
 
     public void testBuild() throws IOException
     {
-        ChannelsCreate channelsCreate = new ChannelsCreate();
-        channelsCreate.setToken("Token");
-        channelsCreate.setName("Channel");
-        channelsCreate.setValidate(true);
-        assertEquals(channelsCreate.getToken(), "Token");
-        assertEquals(channelsCreate.getName(), "Channel");
-        assertTrue(channelsCreate.getValidate());
-        assertEquals(channelsCreate.build(), "token=Token&name=Channel&validate=true");
+        ConversationsKick conversationsKick = new ConversationsKick();
+        conversationsKick.setToken("Token");
+        conversationsKick.setChannel("Channel");
+        conversationsKick.setUser("User");
+        assertEquals(conversationsKick.getToken(), "Token");
+        assertEquals(conversationsKick.getChannel(), "Channel");
+        assertEquals(conversationsKick.getUser(), "User");
+        assertEquals(conversationsKick.build(), "token=Token&channel=Channel&user=User");
     }
 }
