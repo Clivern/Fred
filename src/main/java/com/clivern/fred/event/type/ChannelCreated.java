0c7ecd5de86546cebc0793538cc4885880b9ae19
Clivern <hello@clivern.com>
2017-11-27 15:29:55 +0200

    Channel Created Event Added

--- src/main/java/com/clivern/fred/event/type/ChannelCreated.java
+++ src/main/java/com/clivern/fred/event/type/ChannelCreated.java
@@ -16,9 +16,122 @@ package com.clivern.fred.event.type;
 /**
  * Channel Created Event
  *
+ * Expected scopes: channels:read
+ *
+ * <a href="https://api.slack.com/events/channel_created">For More Info</a>
+ *
  * @author A.F
  * @since 1.0.0
  */
 public class ChannelCreated {
 
+    /**
+     * Set Event Type. It should be channel_created
+     *
+     * @param eventType
+     */
+    public void setEventType(String eventType)
+    {
+        this.setIncomingItem("event.type", eventType);
+    }
+
+    /**
+     * Set Channel Id
+     *
+     * @param channelId
+     */
+    public void setChannelId(String channelId)
+    {
+        this.setIncomingItem("event.channel.id", channelId);
+    }
+
+    /**
+     * Set Channel Name
+     *
+     * @param channelName
+     */
+    public void setChannelName(String channelName)
+    {
+        this.setIncomingItem("event.channel.name", channelName);
+    }
+
+    /**
+     * Set Channel Created
+     *
+     * @param channelCreated
+     */
+    public void setChannelCreated(String channelCreated)
+    {
+        this.setIncomingItem("event.channel.created", channelCreated);
+    }
+
+    /**
+     * Set Channel Creator
+     *
+     * @param channelCreator
+     */
+    public void setChannelCreator(String channelCreator)
+    {
+        this.setIncomingItem("event.channel.creator", channelCreator);
+    }
+
+    /**
+     * Get Event Type. It should be channel_created
+     *
+     * @return String
+     */
+    public String getEventType()
+    {
+        return this.getIncomingItem("event.type", "");
+    }
+
+    /**
+     * Get Channel Id
+     *
+     * @return String
+     */
+    public String getChannelId()
+    {
+        return this.getIncomingItem("event.channel.id", "");
+    }
+
+    /**
+     * Get Channel Name
+     *
+     * @return String
+     */
+    public String getChannelName()
+    {
+        return this.getIncomingItem("event.channel.name", "");
+    }
+
+    /**
+     * Get Channel Created
+     *
+     * @return String
+     */
+    public String getChannelCreated()
+    {
+        return this.getIncomingItem("event.channel.created", "");
+    }
+
+    /**
+     * Get Channel Creator
+     *
+     * @return String
+     */
+    public String getChannelCreator()
+    {
+        return this.getIncomingItem("event.channel.creator", "");
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        return true;
+    }
 }
