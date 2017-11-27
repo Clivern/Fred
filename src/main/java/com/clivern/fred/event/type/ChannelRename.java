73691f2fa69d1534257303c5cd114b7a9c932944
Clivern <hello@clivern.com>
2017-11-27 15:30:18 +0200

    Channel Rename Event Added

--- src/main/java/com/clivern/fred/event/type/ChannelRename.java
+++ src/main/java/com/clivern/fred/event/type/ChannelRename.java
@@ -16,9 +16,102 @@ package com.clivern.fred.event.type;
 /**
  * Channel Rename Event
  *
+ * Expected scopes: channels:read
+ *
+ * <a href="https://api.slack.com/events/channel_rename">For More Info</a>
+ *
  * @author A.F
  * @since 1.0.0
  */
 public class ChannelRename {
 
+    /**
+     * Set Event Type. It should be channel_rename
+     *
+     * @param eventType
+     */
+    public void setEventType(String eventType)
+    {
+        this.setIncomingItem("event.type", eventType);
+    }
+
+    /**
+     * Set Channel Id
+     *
+     * @param channelId
+     */
+    public void setChannelId(String channelId)
+    {
+        this.setIncomingItem("event.channel.id", channelId);
+    }
+
+    /**
+     * Set Channel Name
+     *
+     * @param channelName
+     */
+    public void setChannelName(String channelName)
+    {
+        this.setIncomingItem("event.channel.name", channelName);
+    }
+
+    /**
+     * Set Channel Created
+     *
+     * @param channelCreated
+     */
+    public void setChannelCreated(String channelCreated)
+    {
+        this.setIncomingItem("event.channel.created", channelCreated);
+    }
+
+    /**
+     * Get Event Type. It should be channel_rename
+     *
+     * @return String
+     */
+    public String getEventType()
+    {
+        return this.getIncomingItem("event.type", "");
+    }
+
+    /**
+     * Get Channel Id
+     *
+     * @return String
+     */
+    public String getChannelId()
+    {
+        return this.getIncomingItem("event.channel.id", "");
+    }
+
+    /**
+     * Get Channel Name
+     *
+     * @return String
+     */
+    public String getChannelName()
+    {
+        return this.getIncomingItem("event.channel.name", "");
+    }
+
+    /**
+     * Get Channel Created
+     *
+     * @return String
+     */
+    public String getChannelCreated()
+    {
+        return this.getIncomingItem("event.channel.created", "");
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        return true;
+    }
 }
