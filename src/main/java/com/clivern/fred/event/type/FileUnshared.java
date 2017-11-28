1456da6f84c24d0df78fa7eb345d49ffee750441
Clivern <hello@clivern.com>
2017-11-28 17:01:39 +0200

    File Unshared Event Added

--- src/main/java/com/clivern/fred/event/type/FileUnshared.java
+++ src/main/java/com/clivern/fred/event/type/FileUnshared.java
@@ -13,12 +13,69 @@
  */
 package com.clivern.fred.event.type;
 
+import com.clivern.fred.contract.event.type.EventTemplate;
+
 /**
  * File Unshared Event
+ *
+ * A file was unshared. It Works with RTM & Events API
+ *
+ * Expected scopes: files:read
+ *
+ * <a href="https://api.slack.com/events/file_unshared">For More Info</a>
  *
  * @author A.F
  * @since 1.0.0
  */
-public class FileUnshared {
+public class FileUnshared extends EventTemplate {
 
+    /**
+     * Set Event Type. It should be file_unshared
+     *
+     * @param eventType
+     */
+    public void setEventType(String eventType)
+    {
+        this.setIncomingItem("event.type", eventType);
+    }
+
+    /**
+     * Set File ID
+     *
+     * @param fileId
+     */
+    public void setFileId(String fileId)
+    {
+        this.setIncomingItem("event.file_id", fileId);
+    }
+
+    /**
+     * Get Event Type. It should be file_unshared
+     *
+     * @return String
+     */
+    public String getEventType()
+    {
+        return this.getIncomingItem("event.type", "");
+    }
+
+    /**
+     * Get File ID
+     *
+     * @return String
+     */
+    public String getFileId()
+    {
+        return this.getIncomingItem("event.file_id", "");
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        return true;
+    }
 }
