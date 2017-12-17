38ecd0a57b27248705cce6d7629de8aedc9256a6
Clivern <hello@clivern.com>
2017-12-17 23:57:55 +0200

    Add Misc Related Events

---
+++ src/main/java/com/clivern/fred/event/type/AppUninstalled.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2017 A.F <http://clivern.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.clivern.fred.event.type;
+
+import com.clivern.fred.contract.event.type.EventTemplate;
+
+/**
+ * App Uninstalled Event
+ *
+ * Your Slack app was uninstalled. It Works with Events API
+ *
+ * <a href="https://api.slack.com/events/app_uninstalled">For More Info</a>
+ *
+ * @author A.F
+ * @since 1.0.0
+ */
+public class AppUninstalled extends EventTemplate {
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public AppUninstalled(Function<EventTemplate,String> callback)
+    {
+        super(callback);
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Set Event Type. It should be app_uninstalled
+     *
+     * @param eventType
+     */
+    public void setEventType(String eventType)
+    {
+        this.setIncomingItem("event.type", eventType);
+    }
+
+    /**
+     * Get Event Type. It should be app_uninstalled
+     *
+     * @return String
+     */
+    public String getEventType()
+    {
+        return this.getIncomingItem("event.type", "");
+    }
+}
