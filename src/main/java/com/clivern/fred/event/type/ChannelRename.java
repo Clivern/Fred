30e6d59958c1ae73fb35bcbd7b50c74428eaa1f5
Clivern <hello@clivern.com>
2018-01-02 22:39:07 +0100

    Channel Rename event updated

--- src/main/java/com/clivern/fred/event/type/ChannelRename.java
+++ src/main/java/com/clivern/fred/event/type/ChannelRename.java
@@ -14,6 +14,8 @@
 package com.clivern.fred.event.type;
 
 import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
 
 /**
  * Channel Rename Event
@@ -28,6 +30,52 @@ import com.clivern.fred.contract.event.type.EventTemplate;
  * @since 1.0.0
  */
 public class ChannelRename extends EventTemplate {
+
+    protected Function<ChannelRename,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public ChannelRename(Function<ChannelRename,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
 
     /**
      * Set Event Type. It should be channel_rename
@@ -107,15 +155,5 @@ public class ChannelRename extends EventTemplate {
     public String getChannelCreated()
     {
         return this.getIncomingItem("event.channel.created", "");
-    }
-
-    /**
-     * Parse Event Incoming Data
-     *
-     * @return Boolean
-     */
-    public Boolean parse()
-    {
-        return true;
     }
 }
