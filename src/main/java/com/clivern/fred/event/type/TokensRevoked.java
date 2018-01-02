c0392a9722a2b9ede9420204ce7d1ac0533dfacb
Clivern <hello@clivern.com>
2018-01-02 23:39:47 +0100

    Tokens Revoked Event Added

--- src/main/java/com/clivern/fred/event/type/TokensRevoked.java
+++ src/main/java/com/clivern/fred/event/type/TokensRevoked.java
@@ -15,6 +15,8 @@ package com.clivern.fred.event.type;
 
 import com.clivern.fred.contract.event.type.EventTemplate;
 import java.util.ArrayList;
+import org.json.JSONObject;
+import java.util.function.Function;
 
 /**
  * Tokens Revoked Event
@@ -26,6 +28,52 @@ import java.util.ArrayList;
  */
 public class TokensRevoked extends EventTemplate {
 
+    protected Function<TokensRevoked,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public TokensRevoked(Function<TokensRevoked,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
+    
     /**
      * Set Event Type. It should be tokens_revoked
      *
@@ -102,15 +150,5 @@ public class TokensRevoked extends EventTemplate {
     public String getBotTokens()
     {
         return this.getIncomingItem("tokens.bot", "");
-    }
-
-    /**
-     * Parse Event Incoming Data
-     *
-     * @return Boolean
-     */
-    public Boolean parse()
-    {
-        return true;
     }
 }
