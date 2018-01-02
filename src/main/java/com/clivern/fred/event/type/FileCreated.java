f3bfde92019767a3bc2bc19019691716127793e3
Clivern <hello@clivern.com>
2018-01-02 23:32:32 +0100

    File Created Event Updated

--- src/main/java/com/clivern/fred/event/type/FileCreated.java
+++ src/main/java/com/clivern/fred/event/type/FileCreated.java
@@ -14,6 +14,8 @@
 package com.clivern.fred.event.type;
 
 import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
 
 /**
  * File Created Event
@@ -29,6 +31,52 @@ import com.clivern.fred.contract.event.type.EventTemplate;
  */
 public class FileCreated extends EventTemplate {
 
+    protected Function<FileCreated,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public FileCreated(Function<FileCreated,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
+    
     /**
      * Set Event Type. It should be file_created
      *
@@ -67,15 +115,5 @@ public class FileCreated extends EventTemplate {
     public String getFileId()
     {
         return this.getIncomingItem("event.file_id", "");
-    }
-
-    /**
-     * Parse Event Incoming Data
-     *
-     * @return Boolean
-     */
-    public Boolean parse()
-    {
-        return true;
     }
 }
