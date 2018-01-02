ed87d21fd7be675c4cc4a0d7e505c39f0ff1acb9
Clivern <hello@clivern.com>
2018-01-02 22:41:35 +0100

    Channel Unarchive event updated

--- src/main/java/com/clivern/fred/event/type/ChannelUnarchive.java
+++ src/main/java/com/clivern/fred/event/type/ChannelUnarchive.java
@@ -14,6 +14,8 @@
 package com.clivern.fred.event.type;
 
 import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
 
 /**
  * Channel Unarchive Event
@@ -28,6 +30,52 @@ import com.clivern.fred.contract.event.type.EventTemplate;
  * @since 1.0.0
  */
 public class ChannelUnarchive extends EventTemplate {
+
+    protected Function<ChannelUnarchive,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public ChannelUnarchive(Function<ChannelUnarchive,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
 
     /**
      * Set Event Type. It should be channel_unarchive
@@ -87,15 +135,5 @@ public class ChannelUnarchive extends EventTemplate {
     public String getUser()
     {
         return this.getIncomingItem("event.user", "");
-    }
-
-    /**
-     * Parse Event Incoming Data
-     *
-     * @return Boolean
-     */
-    public Boolean parse()
-    {
-        return true;
     }
 }
