1970ea273e18378e77f2b827f62406ad94f201c2
Clivern <hello@clivern.com>
2018-01-02 22:33:51 +0100

    Channel Deleted Event Updated

--- src/main/java/com/clivern/fred/event/type/ChannelDeleted.java
+++ src/main/java/com/clivern/fred/event/type/ChannelDeleted.java
@@ -14,6 +14,8 @@
 package com.clivern.fred.event.type;
 
 import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
 
 /**
  * Channel Deleted Event
@@ -29,15 +31,50 @@ import com.clivern.fred.contract.event.type.EventTemplate;
  */
 public class ChannelDeleted extends EventTemplate {
 
+    protected Function<ChannelDeleted,String> callback;
 
     /**
      * Class Constructor
      *
-     * @param event
+     * @param callback
      */
-    public UrlVerification(String event)
+    public ChannelDeleted(Function<ChannelDeleted,String> callback)
     {
-        super(event);
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
     }
 
     /**
@@ -78,15 +115,5 @@ public class ChannelDeleted extends EventTemplate {
     public String getChannel()
     {
         return this.getIncomingItem("event.channel", "");
-    }
-
-    /**
-     * Parse Event Incoming Data
-     *
-     * @return Boolean
-     */
-    public Boolean parse()
-    {
-        return true;
     }
 }
