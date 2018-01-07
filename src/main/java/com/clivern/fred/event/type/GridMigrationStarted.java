ecd76841c34070463138e02333101273ea64626e
Clivern <hello@clivern.com>
2018-01-07 13:04:33 +0100

    Grid Migration Started Event Fixed

--- src/main/java/com/clivern/fred/event/type/GridMigrationStarted.java
+++ src/main/java/com/clivern/fred/event/type/GridMigrationStarted.java
@@ -13,12 +13,61 @@
  */
 package com.clivern.fred.event.type;
 
+import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
+
 /**
  * Grid Migration Started Event
  *
  * @author A.F
  * @since 1.0.0
  */
-public class GridMigrationStarted {
+public class GridMigrationStarted extends EventTemplate {
 
+    protected Function<GridMigrationStarted,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public GridMigrationStarted(Function<GridMigrationStarted,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
 }
