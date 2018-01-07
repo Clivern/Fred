0fd99e6f6e81e866822783f88710d1290d0cd6e2
Clivern <hello@clivern.com>
2018-01-07 13:10:17 +0100

    subteam self added event fixed

--- src/main/java/com/clivern/fred/event/type/SubteamSelfAdded.java
+++ src/main/java/com/clivern/fred/event/type/SubteamSelfAdded.java
@@ -13,12 +13,61 @@
  */
 package com.clivern.fred.event.type;
 
+import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
+
 /**
  * Subteam Self Added Event
  *
  * @author A.F
  * @since 1.0.0
  */
-public class SubteamSelfAdded {
+public class SubteamSelfAdded extends EventTemplate {
 
+    protected Function<SubteamSelfAdded,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public SubteamSelfAdded(Function<SubteamSelfAdded,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
 }
