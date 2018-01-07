572ad7b74d43c8af4f67ce60f44266ef59f9bfb8
Clivern <hello@clivern.com>
2018-01-07 13:06:46 +0100

    Message Event Fixed

--- src/main/java/com/clivern/fred/event/type/Message.java
+++ src/main/java/com/clivern/fred/event/type/Message.java
@@ -13,12 +13,61 @@
  */
 package com.clivern.fred.event.type;
 
+import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
+
 /**
  * Message Event
  *
  * @author A.F
  * @since 1.0.0
  */
-public class Message {
+public class Message extends EventTemplate {
 
+    protected Function<Message,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public Message(Function<Message,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
 }
