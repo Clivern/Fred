79019fe81e8f87745732d7fd1a845b57ae011cd7
Clivern <hello@clivern.com>
2018-01-07 13:10:32 +0100

    subteam self removed event fixed

--- src/main/java/com/clivern/fred/event/type/SubteamSelfRemoved.java
+++ src/main/java/com/clivern/fred/event/type/SubteamSelfRemoved.java
@@ -13,12 +13,61 @@
  */
 package com.clivern.fred.event.type;
 
+import com.clivern.fred.contract.event.type.EventTemplate;
+import org.json.JSONObject;
+import java.util.function.Function;
+
 /**
  * Subteam Self Removed Event
  *
  * @author A.F
  * @since 1.0.0
  */
-public class SubteamSelfRemoved {
+public class SubteamSelfRemoved extends EventTemplate {
 
+    protected Function<SubteamSelfRemoved,String> callback;
+
+    /**
+     * Class Constructor
+     *
+     * @param callback
+     */
+    public SubteamSelfRemoved(Function<SubteamSelfRemoved,String> callback)
+    {
+        this.callback = callback;
+    }
+
+    /**
+     * Check if This Event Is Called
+     *
+     * @return Boolean
+     */
+    public Boolean isCalled()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Parse Event Incoming Data
+     *
+     * @return Boolean
+     */
+    public Boolean parse()
+    {
+        JSONObject requestData = new JSONObject(this.getPlainRequest());
+
+        return true;
+    }
+
+    /**
+     * Call Event Callback
+     *
+     * @return String
+     */
+    public String call()
+    {
+        return this.callback.apply(this);
+    }
 }
