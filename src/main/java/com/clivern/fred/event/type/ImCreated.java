997672ac19c4e71d28a076a2c21455f4a76bb0b9
Clivern <hello@clivern.com>
2018-01-09 12:07:35 +0100

    Im Created Event Updated

--- src/main/java/com/clivern/fred/event/type/ImCreated.java
+++ src/main/java/com/clivern/fred/event/type/ImCreated.java
@@ -46,7 +46,7 @@ public class ImCreated extends EventTemplate {
     {
         JSONObject requestData = new JSONObject(this.getPlainRequest());
 
-        return true;
+        return (requestData.has("type") && requestData.getString("type").equals("im_close"));
     }
 
     /**
@@ -58,6 +58,16 @@ public class ImCreated extends EventTemplate {
     {
         JSONObject requestData = new JSONObject(this.getPlainRequest());
 
+        if( requestData.has("type") && !requestData.getString("type").equals("") ){
+            this.setEventType(requestData.getString("type"));
+        }
+        if( requestData.has("channel") && !requestData.getString("channel").equals("") ){
+            this.setChannel(requestData.getString("channel"));
+        }
+        if( requestData.has("user") && !requestData.getString("user").equals("") ){
+            this.setUser(requestData.getString("user"));
+        }
+        
         return true;
     }
 
@@ -69,5 +79,65 @@ public class ImCreated extends EventTemplate {
     public String call()
     {
         return this.callback.apply(this);
+    }
+    
+    /**
+     * Set Event Type. It should be group_open
+     *
+     * @param eventType
+     */
+    public void setEventType(String eventType)
+    {
+        this.setIncomingItem("event.type", eventType);
+    }
+
+    /**
+     * Set Channel Name
+     *
+     * @param channel
+     */
+    public void setChannel(String channel)
+    {
+        this.setIncomingItem("event.channel", channel);
+    }
+
+    /**
+     * Set User
+     *
+     * @param user
+     */
+    public void setUser(String user)
+    {
+        this.setIncomingItem("event.user", user);
+    }
+
+    /**
+     * Get Event Type. It should be group_open
+     *
+     * @return String
+     */
+    public String getEventType()
+    {
+        return this.getIncomingItem("event.type", "");
+    }
+
+    /**
+     * Get Channel Name
+     *
+     * @return String
+     */
+    public String getChannel()
+    {
+        return this.getIncomingItem("event.channel", "");
+    }
+
+    /**
+     * Get User
+     *
+     * @return String
+     */
+    public String getUser()
+    {
+        return this.getIncomingItem("event.user", "");
     }
 }
